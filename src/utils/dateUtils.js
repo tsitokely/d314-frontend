fb2abaf95208c2841ec83e258ac7694cc07af6f9
Tsitohaina Toetra RAKOTONDRAMASY <tsitokely@users.noreply.github.com>
2024-05-30 00:59:32 -0400

    Updated week to 52

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,6 +1,6 @@
 function populateWeeks(weekSelect1, weekSelect2) {
     // Rajouter les 53 semaines - source: MDN web docs
-    for (let i = 1; i <= 53; i++) {
+    for (let i = 1; i <= 52; i++) {
         const option1 = document.createElement("option");
         option1.textContent = i < 10 ? `0${i}` : i;
         weekSelect2.appendChild(option1);
